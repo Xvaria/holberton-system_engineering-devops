a4a9ba42025f46d07d0df80e8db7ba335df25441
Xvaria <2349@holbertonschool.com>
2021-04-06 03:57:23 +0000

    Ready Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # SSH configuration file so that you can connect to a server without typing a password
 file { 'holberton':
-  path   => '/etc/.ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   ensure => present,
   owner  => 'root',
   group  => 'root',
@@ -8,11 +8,11 @@ file { 'holberton':
 }
 
 file_line { 'Turn off passwd auth':
-  path => '/etc/.ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Declare identity file':
-  path => '/etc/.ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no'
 }
