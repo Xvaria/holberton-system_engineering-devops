df23ca0f5918b635af4e6112be69b8345f594e30
Xvaria <2349@holbertonschool.com>
2021-05-04 01:13:03 +0000

    Ready | Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''using this REST API, for a given employee ID, returns information\
+about his/her TODO list progress'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    csv = ""
+    html = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}users/{}".format(html, argv[1]))
+    json_usr = users.json()
+    todos = requests.get("{}todos".format(html))
+    json_tds = todos.json()
+    for dic in json_tds:
+        if dic['userId'] == int(argv[1]):
+            csv += '"{}","{}","{}","{}"\n'.format(argv[1], json_usr['username'],
+                                                dic['completed'], dic['title'])
+    with open (argv[1]+".csv", "w+") as file:
+        file.write(csv)
